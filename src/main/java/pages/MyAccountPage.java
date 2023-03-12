6f342c026bc20412d4abb294bfd53853fa50e4e5
yuliia soloviova <julysoloviova@gmail.com>
2023-03-12 17:13:09 -0300

    My account options test.

--- src/main/java/pages/MyAccountPage.java
+++ src/main/java/pages/MyAccountPage.java
@@ -1,4 +1,125 @@
 package pages;
 
-public class MyAccountPage {
+import libs.TestData;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import pages.elements.HeaderElements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAccountPage extends ParentPage {
+    private HeaderElements headerElements = new HeaderElements(webDriver);
+
+    @FindBy(xpath = ".//div[@class='utility-bar desktop-only']//a[@title='My Account']")
+    private WebElement myAccount;
+    @FindBy(xpath = ".//div[@class='utility-bar desktop-only']//a[@title='My rewards']")
+    private WebElement myRewards;
+    @FindBy(xpath = ".//div[@class='utility-bar desktop-only']//a[@title='My payment settings']")
+    private WebElement myPaymentSettings;
+    @FindBy(xpath = ".//div[@class='utility-bar desktop-only']//a[@title='My orders']")
+    private WebElement myOrders;
+    @FindBy(xpath = ".//div[@class='utility-bar desktop-only']//a[@title='My profile']")
+    private WebElement myProfile;
+    @FindBy(xpath = ".//div[@class='utility-bar desktop-only']//a[@title='My addresses']")
+    private WebElement myAddresses;
+    @FindBy(xpath = ".//div[@class='utility-bar desktop-only']//a[@title='My communication preferences']")
+    private WebElement myCommunicationPreferences;
+    @FindBy(xpath = ".//div[@class='utility-bar desktop-only']//a[@title='Logout']")
+    private WebElement logOutOption;
+    private static final String navElement = ".//nav//a[@class='has-sub-menu top-level-tag']";
+    @FindBy(xpath = navElement)
+    private List<WebElement> listOfOptions;
+
+    public MyAccountPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public HeaderElements getHeaderElements() {
+        return headerElements;
+    }
+
+    public MyAccountPage openMyAccountPage() {
+        LandingPage landingPage = new LandingPage(webDriver);
+        landingPage.openLandingPage();
+        if (!getHeaderElements().isUserNameLogoDisplayed(TestData.USER_NAME)) {
+            landingPage.fillInLogInForm();
+        }
+        checkIsRedirectedToMyAccountPage();
+        return this;
+    }
+
+    public MyAccountPage checkIsRedirectedToMyAccountPage() {
+        Assert.assertTrue("Element is not displayed", getHeaderElements().isUserNameLogoDisplayed(TestData.USER_NAME));
+        return this;
+    }
+
+    public MyAccountPage checkMyAccountOptionPresent() {
+        webDriverWait15.until(ExpectedConditions.visibilityOf(myAccount));
+        checkDisplayedElementOnly(myAccount);
+        return this;
+    }
+
+    public MyAccountPage checkMyRewadsOptionPresent() {
+        webDriverWait15.until(ExpectedConditions.visibilityOf(myRewards));
+        checkDisplayedElementOnly(myRewards);
+        return this;
+    }
+
+    public MyAccountPage checkMyPaymentOptionPresent(String myPaymentOption) {
+        forTextComparing(myPaymentOption, myPaymentSettings);
+        return this;
+    }
+
+    public MyAccountPage checkMyOrdersOptionPresent() {
+        webDriverWait15.until(ExpectedConditions.visibilityOf(myOrders));
+        checkDisplayedElementOnly(myOrders);
+        return this;
+    }
+
+    public MyAccountPage checkMyProfileOptionPresent() {
+        checkDisplayedElementOnly(myProfile);
+        return this;
+    }
+
+    public MyAccountPage checkMyAddresses() {
+        checkDisplayedElementOnly(myAddresses);
+        return this;
+    }
+
+    public MyAccountPage checkMyCommunicationPreferencesPresent(String myCommPref) {
+        forTextComparing(myCommPref, myCommunicationPreferences);
+        return this;
+    }
+
+    public MyAccountPage checkLogOutOptionPresent() {
+        checkDisplayedElementOnly(logOutOption);
+        return this;
+    }
+
+    public MyAccountPage checkShopNavigationOptions(String expectedElements) {
+        String[] expectedWebElements = expectedElements.split(",");
+        webDriverWait15.withMessage("Number of messages must be " + expectedWebElements.length)
+                .until(ExpectedConditions.numberOfElementsToBe(By.xpath(navElement), expectedWebElements.length));
+        Assert.assertEquals(" Number of elements ", expectedWebElements.length, listOfOptions.size());
+        logger.info(expectedWebElements.length + " options are available to the user.");
+
+        ArrayList<String> valueOfOptions = new ArrayList<>();
+        for (WebElement webElement : listOfOptions) {
+            valueOfOptions.add(webElement.getText());
+            logger.info(webElement.getText());
+        }
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        for (int i = 0; i < expectedWebElements.length; i++) {
+            softAssertions.assertThat(expectedWebElements[i]).as("Element does not find its match ").isIn(valueOfOptions);
+        }
+        softAssertions.assertAll();
+        return this;
+    }
 }
--- src/main/java/pages/elements/HeaderElements.java
+++ src/main/java/pages/elements/HeaderElements.java
@@ -1,10 +1,15 @@
 package pages.elements;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import pages.InteractionsWithElements;
+import pages.MyAccountPage;
 
 public class HeaderElements extends InteractionsWithElements {
     private String userNameLogo = ".//div[@class='utility-bar desktop-only']//a[@href='https://www.ardene.com/ca/en/account'and @title='User: Hello %s']";
+    @FindBy(xpath = ".//div[@class='utility-bar desktop-only']//span[@class='span-icon']")
+    private WebElement customDropDown;
 
     public HeaderElements(WebDriver webDriver) {
         super(webDriver);
@@ -12,5 +17,10 @@ public class HeaderElements extends InteractionsWithElements {
 
     public boolean isUserNameLogoDisplayed(String userName) {
         return isElementDisplayed(String.format(userNameLogo,userName));
+    }
+
+    public MyAccountPage clickOptionsDropDown(){
+        clickOnElement(customDropDown);
+         return new MyAccountPage(webDriver);
     }
 }
